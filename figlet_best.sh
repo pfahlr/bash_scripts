9626574750d285df3802894066ed1c98951d05d4
Rick Pfahl <pfahlr@gmail.com>
2025-09-07 03:04:01 -0400

    new scripts

--- figlet_best.sh
+++ figlet_best.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 # $1 = fonts directory $2 = string to process (in quotes if contains whitespace) $3 = max output width
 FIGLETCMD="toilet"
-for i in `cat figlet_top_100`; do;
+for i in `cat  /home/rick/bash_scripts/figlet_top_100`; do;
   echo "=============================================================================="
   echo "$i" 
   echo "------------------------------------------------------------------------------"
